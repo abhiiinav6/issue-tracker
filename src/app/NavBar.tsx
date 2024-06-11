eb1305800d519fa4c64e91c1f628e2df10b7ef02
Abhinav <abhinavkumar34002@gmail.com>
2024-06-11 10:15:50 +0530

    styled active link in navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import { Bug } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+    const links = [
+        {
+            label: "Dashboard",
+            href: "/",
+        },
+        {
+            label: "Issues",
+            href: "/issues",
+        },
+    ];
+    const pathname = usePathname();
+    return (
+        <nav className="w-full flex py-4 px-16 h-14 border-b items-center gap-8">
+            <Link href="/">
+                <Bug />
+            </Link>
+            <ul className="flex gap-8 ">
+                {links.map((link) => {
+                    return (
+                        <Link
+                            className={classNames({
+                                "text-zinc-900": pathname===link.href,
+                                "text-zinc-500": pathname!==link.href,
+                                "transition-colors hover:text-zinc-900 cursor-pointer": true
+                            })}
+                            key={link.label}
+                            href={link.href}
+                        >
+                            {link.label}
+                        </Link>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+};
 
-export default NavBar
+export default NavBar;

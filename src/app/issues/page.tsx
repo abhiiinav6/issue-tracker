8ef80ad6a7e1bab8c1d990ac41da25909e4eb035
Abhinav <abhinavkumar34002@gmail.com>
2024-06-11 11:14:29 +0530

    Added radix ui

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const page = () => {
-  return (
-    <div>issues</div>
-  )
-}
+    return (
+        <div>
+            <Button>New Issue</Button>
+        </div>
+    );
+};
 
-export default page
+export default page;
